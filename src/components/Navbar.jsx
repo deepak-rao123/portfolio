9cf783bda915fc3a3fe58ee7e094f2269e6a9c2b
deepak-rao123 <dy436387@gmail.com>
2024-08-01 17:50:08 +0530

    contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import {NavLink} from "react-router-dom"
 const Navbar = () => {
   return (
    <header className="header">
-    <NavLink to="/" className='w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md'>
-    <p className="blue-gradient_text">AH</p>
+    <NavLink to="/" className='w-65 h-9 rounded-lg bg-white flex items-center justify-center font-bold shadow-md'>
+    <p className="blue-gradient_text">Deepak Yadav</p>
 
     </NavLink>
     <nav className="flex text-lg gap-7 font-medium">
